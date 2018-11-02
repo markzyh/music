4a07ece8c429b39c8b67814ec092595f4fa62284
markzyh <980511383@qq.com>
2018-11-02 23:46:01 +0800

    nothing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+* { box-sizing:border-box; -webkit-box-sizing:border-box; -moz-box-sizing:border-box; font-family: "幼圆"; margin:0; padding:0;}
+body { line-height: 1.6em; color: #333; font-size: 16px;background: #f0f2f5;}
+a { text-decoration: none; color: #333; border:none; outline: none;}
+a:focus,img { border:none;}
+a:focus { outline: none;}
+ul,li { list-style: none;}
+input,select { border: none; -webkit-appearance:none;}
+table { border-collapse: collapse;border-spacing: 0;}
+h1, h2, h3, h4, h5, h6 { font-weight: normal; }
+em, i, address {font-style: normal;}
+.clear { zoom:1;}
+.clear:after { height: 0; display: block; content: "\20"; clear: both;}
+.fl { float: left;}
+.fr { float: right;}
+.mc { margin:0 auto;}
+.dis-inline { display: inline-block; vertical-align: middle;}
+.text-center { text-align: center;}
+.table-cell { vertical-align: middle; display: table-cell; }
 </style>
